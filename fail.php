418d0e1e5e63b7d764247db7b3c9877fb31c1c10
martkasa <martkasa@greeny.cs.tlu.ee>
2016-09-06 15:16:49 +0300

    fail

---
+++ fail.php
@@ -0,0 +1,18 @@
+
+
+<?php
+
+	$firstName = "Martin";
+	$lastName = "Kasak";
+	
+	//echo trükib välja muutja veebilehel
+	
+	//echo $firstName;
+	//echo " ";	
+	//echo $lastName;
+	
+	// punkt liidab stringe
+	
+	echo $firstName. " " .$lastName;
+?>
+
